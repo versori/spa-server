73b77558895a6a6398075b8b1b240ace4b1bd34f
dan-j <5727701+dan-j@users.noreply.github.com>
2022-09-19 23:16:47 +0100

    chore: add lint-staged to prevent accidental linting errors

--- src/client/__tests__/load-config.test.ts
+++ src/client/__tests__/load-config.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from "@jest/globals";
+import { beforeEach, describe, expect, it } from '@jest/globals';
 import { getConfig } from '../config';
 import { loadConfig } from '../load-config';
 
